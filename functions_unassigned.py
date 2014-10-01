e14102ba172f1affedab1535631ce6d64481e833
Kyle Bridgemohansingh <bsingh.kyle@gmail.com>
2014-10-01 18:51:37 -0400

    started fns

---
+++ functions_unassigned.py
@@ -0,0 +1,40 @@
+'''Site FNs '''
+
+def AddSite(Monitor, Name, URL, Influence):
+    pass
+    #return none
+
+def GetSite(Name):
+    pass
+    #return site
+
+def SetSite(Name, Field, Value):
+    pass
+    #return none
+
+def DelSite(Name):
+    pass
+    #return None
+
+''' Keyword fns'''
+
+def AddKeyword(Keyword):
+    pass
+    #return none
+
+def DelKeyword(Keyword):
+    pass
+    #return None
+
+def GetKeywords():
+    pass
+    #return list of keywords
+
+
+def FindArtciles(allMonitoredSites):
+    pass
+    #forloop(monitored sites):
+        #SitesToArticles(Name, foreignSItes):
+            #article tp ward
+            #war to db
+    #return None
