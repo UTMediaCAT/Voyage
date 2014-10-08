a07e35ccfb8d3653105e6dd7035a84ca685907f4
Yuya <yuya.iwabuchi@gmail.com>
2014-10-08 17:31:41 -0400

    Organized DB related functions and Explorer

---
+++ DBManager.py
@@ -0,0 +1,52 @@
+
+''' INPUTS '''
+
+#Site functions
+
+def add_site(Monitor, Name, URL, Influence):
+    pass
+    #return none
+
+def get_site(Name):
+    pass
+    #return site
+
+def set_site(Name, Field, Value):
+    pass
+    #return none
+
+def del_site(Name):
+    pass
+    #return None
+
+#Keyword functions
+
+def add_keyword(Keyword):
+    pass
+    #return none
+
+def del_keyword(Keyword):
+    pass
+    #return None
+
+def get_keywords():
+    pass
+    #return list of keywords
+
+
+''' OUTPUTS '''
+
+#Article functions
+
+def add_article(url, title, date, author, keywords, sources):
+    pass
+    #return None
+
+def get_article(field, value):
+    pass
+    #return list of articles
+
+def del_article(url):
+    pass
+    #return none
+
---
+++ Explorer.py
@@ -0,0 +1,12 @@
+
+import newspaper
+
+
+
+def find_artciles(allMonitoredSites):
+    pass
+    #forloop(monitored sites):
+        #SitesToArticles(Name, foreignSItes):
+            #article tp ward
+            #war to db
+    #return None
