3904008b77f8e95496e2cfbe3ca86f64a56e1c50
Kyle Bridgemohansingh <bsingh.kyle@gmail.com>
2014-10-12 03:57:07 -0400

    Updated twitter_explorer.py
    Made the decision to use tweepy, as I believe its a better fit for what
    we need.
    
    Implemented get_recent_tweets(), get_followers() tweets.

--- twitter_explorer.py
+++ twitter_explorer.py
@@ -1,1 +1,42 @@
 import db_manager
+import tweepy
+
+# Twitter Developer API
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+    
+def get_recent_tweets(user, amount=20):
+    ''' (str, [int]) -> list of list
+    Gets amount tweets from specified users
+    Returns list in format [uni tweet, uni user, str time_tweeted]
+    '''
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    new = api.user_timeline(screen_name = user, count = amount)
+
+    tweet_list = []
+
+    for tweet in new:
+        tweet_list.append([tweet.text ,tweet.user.screen_name ,str(tweet.created_at)])
+    print tweet_list
+
+def get_followers(user):
+    ''' (str) -> list
+    Gets usernames of all the twitter accounts following user
+    Returns list in format [int followers, [list of followers]]
+    '''
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    followers = []
+    count = 0
+    for user in tweepy.Cursor(api.followers, screen_name=user).items():
+        count += 1
+        followers.append(user.screen_name)
+    return [count, followers]
