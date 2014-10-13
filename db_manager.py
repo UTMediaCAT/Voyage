96ae9de55c110f5a4e4550ba5bf6daa284e9ae39
wangx173 <rogerxiang.wang@gmail.com>
2014-10-13 15:44:46 -0400

    added connect and close functions in db_manager

--- db_manager.py
+++ db_manager.py
@@ -1,23 +1,46 @@
+from pymongo import *
 
 ''' INPUTS '''
+
+# Database Information
+DB_USER_ID = "acme"
+DB_USER_PSW = "cscc01"
+DB_URL = "mongodb://" + DB_USER_ID + ":" + DB_USER_PSW + "@ds039960.mongolab.com:39960/cscc01-acme"
+DB_NAME = "cscc01-acme"
+
+# HELPER functions (connect)
+def connect(collection_name):
+    '''Connect to database'''
+    global client
+    client = MongoClient(DB_URL)
+    db = client[DB_NAME]
+    global COLLECTION
+    COLLECTION = db[collection_name]
+    print(COLLECTION.find_one())
+
+def close_connection():
+    client.close()
 
 #Site functions
 
 def add_site(Monitor, Name, URL, Influence):
-    pass
-    #return none
+    
+
 
 def get_site(Name):
     pass
     #return site
 
+
 def set_site(Name, Field, Value):
     pass
     #return none
 
+
 def del_site(Name):
     pass
     #return None
+
 
 #Keyword functions
 
@@ -25,9 +48,11 @@ def add_keyword(Keyword):
     pass
     #return none
 
+
 def del_keyword(Keyword):
     pass
     #return None
+
 
 def get_keywords():
     pass
@@ -42,9 +67,11 @@ def add_article(url, title, date, author, keywords, sources):
     pass
     #return None
 
+
 def get_article(field, value):
     pass
     #return list of articles
+
 
 def del_article(url):
     pass
