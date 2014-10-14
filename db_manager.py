0caa72b00bf1e3b87ef487cc30352dd98ed57987
wangx173 <rogerxiang.wang@gmail.com>
2014-10-13 20:26:02 -0400

    implemented db manager

--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,6 @@
 from pymongo import *
 
-''' INPUTS '''
+
 
 # Database Information
 DB_USER_ID = "acme"
@@ -8,72 +8,72 @@ DB_USER_PSW = "cscc01"
 DB_URL = "mongodb://" + DB_USER_ID + ":" + DB_USER_PSW + "@ds039960.mongolab.com:39960/cscc01-acme"
 DB_NAME = "cscc01-acme"
 
-# HELPER functions (connect)
+# connection functions
 def connect(collection_name):
-    '''Connect to database'''
+    '''(str) -> None
+    Connect to database and get a collection by collection_name
+    '''
     global client
     client = MongoClient(DB_URL)
     db = client[DB_NAME]
     global COLLECTION
     COLLECTION = db[collection_name]
-    print(COLLECTION.find_one())
 
 def close_connection():
+    '''(None) -> None
+    Close the connection to database
+    '''
     client.close()
 
-#Site functions
+# universal functions fot sites, articles sites and tweets
 
-def add_site(Monitor, Name, URL, Influence):
-    
+def add_document(new_document):
+    '''(dict) -> None
+    Add new_document to COLLECTION
+    '''
+    COLLECTION.insert(new_document)
+
+def get_document(url):
+    '''(str) -> dict
+    Return a document from COLLECTION by given url
+    '''
+    document = COLLECTION.find_one({"_id":url})
+    return document
 
 
-def get_site(Name):
-    pass
-    #return site
+def set_field_value(url, field, value):
+    '''(str, str, anyType) -> None
+    Set the the value of field of the document given by url
+    '''
+    COLLECTION.update({'_id':url},{'$set': {field:value}})
 
 
-def set_site(Name, Field, Value):
-    pass
-    #return none
 
-
-def del_site(Name):
-    pass
-    #return None
+def delete_document(url):
+    '''(str) -> None
+    Delete the document given by url from the COLLECTION
+    '''
+    COLLECTION.remove(url)
 
 
 #Keyword functions
 
-def add_keyword(Keyword):
-    pass
-    #return none
+def add_keywords(key_word):
+    '''(str) -> None
+    Add a new key_word to the list in COLLECTION
+    '''
+    COLLECTION.update({'_id':1},{'$push': {"list":key_word}})
 
-
-def del_keyword(Keyword):
-    pass
-    #return None
 
 
 def get_keywords():
-    pass
-    #return list of keywords
+    '''(None) - > list of str
+    Return a list of keywords in COLLECTION
+    '''
+    list = COLLECTION.findone["list"]
+    return list
 
 
-''' OUTPUTS '''
-
-#Article functions
-
-def add_article(url, title, date, author, keywords, sources):
-    pass
-    #return None
 
 
-def get_article(field, value):
-    pass
-    #return list of articles
-
-
-def del_article(url):
-    pass
-    #return none
 
