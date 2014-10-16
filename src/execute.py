8613f54c32fd5cee9ab76f8af6f4684810a3dc70
Jai Sughand <jaisughand@gmail.com>
2014-10-16 13:38:46 -0400

    Added get and set function in execute.py

--- src/execute.py
+++ src/execute.py
@@ -31,6 +31,63 @@ def add_foreign_site(url, name, influence):
     '''
     add_site(url, name, False, influence)
 
+def get_all_sites():
+    '''(None) -> list of dict of objects
+    Connect to sites collection and returns all the sites stored in the database
+    '''
+    db.connect('sites')
+    sites = db.get_all_documents()
+    db.close_connection()
+    return sites
+
+def get_monitor_sites():
+    '''(None) -> list of dict of objects
+    Connect to sites collection and returns all the monitor sites stored in the database
+    '''
+    db.connect('sites')
+    sites = db.get_documents("is_monitor", True)
+    db.close_connection()
+    return sites
+
+def get_foreign_sites():
+    '''(None) -> list of dict of objects
+    Connect to sites collection and returns all the foreign sites stored in the database
+    '''
+    db.connect('sites')
+    sites = db.get_documents("is_monitor", False)
+    db.close_connection()
+    return sites
+
+def get_sites(field, value):
+    '''(str, object) -> list of dict of objects
+    Connect to sites collection and returns all the sites stored in the
+    database that match the field and value query
+    Field       : Value
+    "_id"       : str
+    "name"      : str
+    "is_monitor": bool
+    "influence" : int
+    '''
+    db.connect('sites')
+    sites = db.get_documents(field, value)
+    db.close_connection()
+    return sites
+
+def set_site(url, field, new_value):
+    '''(str, str, object) -> None
+    Connect to sites collection and changes finds the site with id as url
+    then changes the field with the new_value
+    Field       : Value
+    "_id"       : str
+    "name"      : str
+    "is_monitor": bool
+    "influence" : int
+    '''
+    db.connect('sites')
+    db.set_field_value(url, field, new_value)
+    print field + " of " + url + " changed to " + str(new_value)
+    db.close_connection()
+
 def remove_site(url):
     '''(str) -> None
     Connect to sites collection and removes a site specified by url
@@ -62,5 +119,22 @@ if __name__ == '__main__':
     add_site("http://time.com", "TIME", True, 2)
     add_site("http://aljazeera.com/", "Al Jazeera", False, 2)
     add_site("http://ynetnews.com", "Ynetnews", False, 2)
+    set_site("http://cnn.com", "influence", 3)
     remove_site("https://news.google.com/")
     #remove_sites()     # Removes all sites in the collection
+
+    print "\n"
+    for site in get_all_sites():
+        print site
+
+    print "\n"
+    for site in get_monitor_sites():
+        print site
+
+    print "\n"
+    for site in get_foreign_sites():
+        print site
+
+    print "\n"
+    for site in get_sites("influence", 2):
+        print site
