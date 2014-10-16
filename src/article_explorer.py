649576f8e9c5a5829b12a69153a39d444e9028ca
Yuya <yuya.iwabuchi@gmail.com>
2014-10-16 05:35:56 -0400

    PEP8

--- src/article_explorer.py
+++ src/article_explorer.py
@@ -26,8 +26,9 @@ from dateutil import parser
 #db_manager
 import db_manager as db
 
+
 def run(keyword_db, site_db, article_db):
-    ''' (str, str, str) -> None
+    """ (str, str, str) -> None
     Connects to keyword and site database, crawls within monitoring sites,
     then pushes articles which matches the keywords or foreign sites to the article database
 
@@ -35,8 +36,7 @@ def run(keyword_db, site_db, article_db):
     keyword_db      -- Keywords database name
     site_db         -- Sites database name
     article_db      -- Article database name
-    '''
-
+    """
 
     print "+----------------------------------------------------------+"
     print "| Retrieving data from Database ...                        |"
@@ -47,19 +47,19 @@ def run(keyword_db, site_db, article_db):
 
     monitoring_sites = []
     # Retrieve, store, and print monitoring site information
-    print "\nMonitoring Sites\n\t%-25s%-40s"%("Name", "URL")
+    print "\nMonitoring Sites\n\t%-25s%-40s" % ("Name", "URL")
     for site in db.get_document("is_monitor", True):
         # monitoring_sites is now in form [['Name', 'URL'], ...]
         monitoring_sites.append([site['name'], site['_id']])
-        print("\t%-25s%-40s"%(site['name'], site['_id']))
+        print("\t%-25s%-40s" % (site['name'], site['_id']))
 
     foreign_sites = []
     # Retrieve, store, and print foreign site information
-    print "\nForeign Sites\n\t%-25s%-40s"%("Name", "URL")
+    print "\nForeign Sites\n\t%-25s%-40s" % ("Name", "URL")
     for site in db.get_document("is_monitor", False):
         # foreign_sites is now in form ['URL', ...]
         foreign_sites.append(site['_id'])
-        print("\t%-25s%-40s"%(site['name'], site['_id']))
+        print("\t%-25s%-40s" % (site['name'], site['_id']))
         
     # Close connection with Site Database
     db.close_connection()
@@ -73,7 +73,7 @@ def run(keyword_db, site_db, article_db):
     db.close_connection()
     print "\nKeywords:"
     for key in keywords:
-        print "\t%s"%key
+        print "\t%s" % key
 
     print "\n"
 
@@ -92,22 +92,21 @@ def run(keyword_db, site_db, article_db):
     parse_articles(populated_sites, keywords, foreign_sites, article_db)
 
 
-
 def populate_sites(sites, from_start):
-    ''' (list of str, bool) -> list of [str, newspaper.source.Source]
+    """ (list of str, bool) -> list of [str, newspaper.source.Source]
     Searches through the sites using newspaper library and
     returns list of sites with available articles populated
 
     Keyword arguments:
     sites         -- List of [name, url] of each site
     from_start    -- Boolean to search sites from scratch
-    '''
+    """
     new_sites = []
     
     # Populate each Sites, then print the amount of articles and time it took
-    print "\n\t%-25s%10s%10s"%("Site", "Articles", "Time")
+    print "\n\t%-25s%10s%10s" % ("Site", "Articles", "Time")
     for s in range(len(sites)):
-        print ("\t%-24s"%(sites[s][0])),
+        print("\t%-24s" % (sites[s][0])),
         # To count the time
         start = time.time()
         # Duplicate the name of the sites
@@ -121,12 +120,13 @@ def populate_sites(sites, from_start):
                                              language='en')))
         end = time.time()
         # report back the amount of articles found, and time it took
-        print ("%6i pgs%9is"%(new_sites[s][1].size(), end - start))
+        print("%6i pgs%9is" % (new_sites[s][1].size(), end - start))
     # return the list
     return new_sites
 
+
 def parse_articles(populated_sites, db_keywords, foreign_sites, db_name):
-    ''' (list of [str, newspaper.source.Source], list of str, list of str, str) -> None
+    """ (list of [str, newspaper.source.Source], list of str, list of str, str) -> None
     Download all articles from built sites and stores the metadata
     to the database
 
@@ -134,8 +134,8 @@ def parse_articles(populated_sites, db_keywords, foreign_sites, db_name):
     populated_sites -- List of [name, 'built_article'] of each site
     total_threads   -- Number of threads to use for downloading per sites.
                        This can greatly increase the speed of download
-    '''
-    added, failed,no_match = 0,0,0
+    """
+    added, failed, no_match = 0, 0, 0
     start = time.time()
     
     # connect to Article Database
@@ -146,9 +146,9 @@ def parse_articles(populated_sites, db_keywords, foreign_sites, db_name):
     
     # for each article in each sites, download and parse important data
     for site in populated_sites:
-        print "\n%s"%site[0]
+        print "\n%s" % site[0]
         for art in site[1].articles:
-            url =  art.url
+            url = art.url
             print "\n\tURL:      ", url
             print "\tEvaluating ...\r",
             # Try to download and extract the useful data
@@ -157,7 +157,7 @@ def parse_articles(populated_sites, db_keywords, foreign_sites, db_name):
                 art.parse()
                 title = art.title
             except:
-                title == ""
+                title = ""
             # If downloading/parsing the page fails, stop here and move on to next article
             if not ((title == "") or (title == "Page not found")):
                 # Regex the keyword from the article's text
@@ -188,8 +188,6 @@ def parse_articles(populated_sites, db_keywords, foreign_sites, db_name):
                     # Most common errors are document already existing, thus print accordingly
                     except:
                         print "\tResult:    Match detected! Article already in the database."
-
-
                 else:
                     no_match += 1
                     print "\tResult:    No Match Detected."
@@ -197,20 +195,20 @@ def parse_articles(populated_sites, db_keywords, foreign_sites, db_name):
                 print "\tResult:    Failed to download!"
                 failed += 1
             # Some stats to look at while running the script
-            print ("\nStatistics\n\tAdded: %i pgs  Failed: %i pgs  No Match: %i pgs  Time Elapsed: %is"%
-                   (added,failed,no_match, time.time() - start))
+            print("\nStatistics\n\tAdded: %i pgs  Failed: %i pgs  No Match: %i pgs  Time Elapsed: %is" %
+                  (added, failed, no_match, time.time() - start))
             print "+--------------------------------------------------------------------+"
 
 
 def get_sources(html, sites):
-    ''' (str, list of str) -> list of str
+    """ (str, list of str) -> list of str
     Searches and returns links redirected to sites within the html
     Returns empty list if none found
 
     Keyword arguments:
     html            -- string of html
     sites           -- List of site urls to look for
-    '''
+    """
     matched_urls = []
 
     # for each site, check if it exists within the html given
@@ -222,15 +220,14 @@ def get_sources(html, sites):
     return matched_urls
 
 
-
 def get_pub_date(article):
-    ''' (newspaper.article.Article) -> str
+    """ (newspaper.article.Article) -> str
     Searches and returns date of which the article was published
     Returns 'N/A' otherwise
 
     Keyword arguments:
     article         -- 'Newspaper.Article' object of article
-    '''
+    """
     dates = []
 
     # For each metadata stored by newspaper's parsing ability, check if any of the key contains 'date'
@@ -249,15 +246,16 @@ def get_pub_date(article):
         return min(dates)
     return 'N/A'
 
+
 def get_keywords(article, keywords):
-    ''' (newspaper.article.Article, list of str) -> list of str
+    """ (newspaper.article.Article, list of str) -> list of str
     Searches and returns keywords which the article contained
     Returns empty list otherwise
 
     Keyword arguments:
     article         -- 'Newspaper.Article' object of article
     keywords        -- List of keywords
-    '''
+    """
     matched_keywords = []
 
     # For each keyword, check if article's text contains it
