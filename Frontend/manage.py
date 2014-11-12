84b96dd5c30b1d8a971dcb1288a78c2e1a452717
Yuya <yuya.iwabuchi@gmail.com>
2014-11-12 05:52:54 -0500

    Restrict path inserting for linux

--- Frontend/manage.py
+++ Frontend/manage.py
@@ -4,7 +4,8 @@ import sys
 
 if __name__ == "__main__":
     # To force local copy of Django
-    sys.path.insert(0, os.path.join(os.environ['HOME'], '.local/lib/python2.7/site-packages'))
+    if os.name != 'nt':
+    	sys.path.insert(0, os.path.join(os.environ['HOME'], '.local/lib/python2.7/site-packages'))
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Frontend.settings")
 
