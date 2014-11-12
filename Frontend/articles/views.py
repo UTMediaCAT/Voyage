e93bef2c8ae0f6beceb0dbde5dea97e028eed7f5
Jai Sughand <jaisughand@gmail.com>
2014-11-12 05:37:11 -0500

    Added a test button to run script using UI

--- Frontend/articles/views.py
+++ Frontend/articles/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
-
+from subprocess import Popen
 from articles.models import Article
-
+import sys, os
 
 def index(request):
     latest_article_list = Article.objects.order_by('date_added')
@@ -16,7 +16,10 @@ def index(request):
     #     'latest_question_list': latest_article_list,
     # })
     # return HttpResponse(template.render(context))
+    
+    path = os.path.dirname(os.path.realpath(__file__))
 
+    #Popen(["python", path + "/jai.py"], cwd=path)
 
     context = {'latest_article_list': latest_article_list}
     return render(request, 'articles/index.html', context)
