4ab0fa8b4e83ccd62f36478399dd3333ccacfa67
ryanbelt <ryanbelt1993129@Hotmail.com>
2014-11-12 23:16:21 -0500

    update setup.py
    added comment and follow PEP8

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 
 import os
 import subprocess
-#+=========GUI===========GUI============GUI===========+
-
-#import os
-#sys=os.path.dirname(os.path.dirname(__file__))
-#print(sys)
 from Tkinter import *
 from tkMessageBox import *
 import webbrowser
-
-
+#+=========GUI===========GUI============GUI===========+
 #---Window---#
 #make window
 window = Tk()
@@ -20,43 +14,51 @@ window.title("Acme Exceute")
 window.geometry("200x200")
 
 #---Commands---#
-#go
-
-
-#---Widgets---#
-#buttons
-
+#Install API command
 def InstallApi():
+    '''
+    ()-->None
+    allow using python connect to CMD and run the sh script to install all api using pip
+    '''
     os.chmod('./src/InstallScript.sh', 0700)
     subprocess.call(['./src/InstallScript.sh'])
     showinfo("congratulation", "All Api sucessfully install")
-Button(window, text="Install Api", command = InstallApi).pack(padx=0)
 
 def Run():
-    os.chmod('./src/RunServer.sh', 0700)
-    #subprocess.call(['./src/RunServer.sh'])
+     '''
+    ()-->None
+    allow using python connect to CMD and run the sh script to open the django server
+    '''
+     os.chmod('./src/RunServer.sh', 0700)
     subprocess.Popen(['./src/RunServer.sh'], shell=True, stdout=subprocess.PIPE)
     showinfo("Notice", "Server on!")
-    #subprocess.Popen(['./src/RunServer.sh'], shell=True, stdout=subprocess.PIPE)
-    
-Button(window, text="Runing Server", command = Run).pack()
 
 def Close():
+     '''
+    ()-->None
+    allow using python connect to CMD and run the sh script to close the django server
+    '''
     os.chmod('./src/CloseServer.sh', 0700)
     subprocess.Popen(['./src/CloseServer.sh'], shell=True, stdout=subprocess.PIPE)
     showinfo("Notice", "Server off!")
-    
-Button(window, text="Close Server", command = Close).pack()
 
 def openweb():
+     '''
+    ()-->None
+    open the django web UI
+    '''
     webbrowser.open('http://127.0.0.1:8000/admin')
-Button(window, text="Open", command = openweb).pack()
-#message
 
-
-#quit
 def quit_app():
     window.destroy()
+
+#---Widgets---#
+#buttons
+Button(window, text="Install Api", command = InstallApi).pack()
+Button(window, text="Runing Server", command = Run).pack()
+Button(window, text="Close Server", command = Close).pack()
+Button(window, text="Open", command = openweb).pack()
 Button(text='quit', command=quit_app).pack()
+
 #+===================GUI END=====================+
 window.mainloop()
