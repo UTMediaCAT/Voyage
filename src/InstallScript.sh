56f8aa43efeb3e15f07eb5212ab5ff0b1350760e
ryan pan <ryanbelt1993129@Hotmail.com>
2014-11-16 21:56:32 -0500

    update install script
    add other install script for warc

--- src/InstallScript.sh
+++ src/InstallScript.sh
@@ -14,3 +14,10 @@ pip install --user tweepy==2.3.0
 pip install --user python-dateutil==1.5
 pip install --user tld==0.7.2
 pip install --user django-suit==0.2.11
+pip install --user Scrapy==0.16.3
+pip install --user pyopenssl==0.13.1
+pip install --user lxml==3.2.3
+pip install --user queuelib==1.0
+pip install --user twisted==12.2.0
+pip install --user w3lib==1.3
+pip install --user zope.interface==4.0.5
