ae1acc3d8dc6f298bfa27f85b284adc6e595f9b1
Jian Heng Pan <panjian2@ic406-154.(none)>
2014-11-19 16:31:45 -0500

    modify create_crawl

--- src/creat_crawl.py
+++ src/creat_crawl.py
@@ -1,26 +1,22 @@
 import os
 import subprocess
-class creat_crawl():
-    def __init__(self):
-        pass
-    
-    def creat_url(self,url):
-        add_url=url
-        crawl = open("Crawl.sh", 'w')
-        crawl.write("#!/bin/bash \n")
-        crawl.write("BASEDIR=$(dirname $0) \n")
-        crawl.write("cd $BASEDIR \n")
-        crawl.write("python crawler.py -u "+url+"\n")
-        crawl.write("cd ..\n")
-        crawl.write("mkdir -p warc\n")
-        crawl.write("mv ./src/out.warc.gz ./warc/\n")
-        crawl.write("cd ./warc \n")
-        add_url=add_url.replace("/","\\")
-        crawl.write("mv out.warc.gz "+add_url+".warc.gz\n")
-        crawl.close()
-        os.chmod('./Crawl.sh', 0700)
-        subprocess.call(['./Crawl.sh'])
+def creat_url(url):
+    add_url=url
+    crawl = open("CreatWarc.sh", 'w')
+    crawl.write("#!/bin/bash \n")
+    crawl.write("BASEDIR=$(dirname $0) \n")
+    crawl.write("cd crawler\n")
+    crawl.write("python crawler.py -u "+url+"\n")
+    crawl.write("cd ..\n")
+    crawl.write("cd ..\n")  
+    crawl.write("mkdir -p warc\n")
+    crawl.write("mv ./src/crawler/out.warc.gz ./warc/\n")
+    crawl.write("cd ./warc \n")
+    add_url=add_url.replace("/","\\\\")
+    crawl.write("mv out.warc.gz "+add_url+".warc.gz\n")
+    crawl.close()
+    os.chmod('./CreatWarc.sh', 0700)
+    subprocess.call(['./CreatWarc.sh'])
 
 #if __name__ == '__main__':
-    #gs=creat_crawl()
-    #gs.creat_url("http://www.facebook.ca")
+    #creat_url("http://www.naturalnews.com/045495_assassination_drones_autonomous_killing_facial_recognition.html")
