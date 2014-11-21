fa27592a86f59dcdd976ad232ba40dbc3c938b42
Xiang Wang <wangx173@ic406-167.(none)>
2014-11-21 14:46:50 -0500

    added charts

--- Frontend/articles/views.py
+++ Frontend/articles/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from subprocess import Popen
-from articles.models import Article
+from articles.models import*
 import sys, os
 
+
+
+
 def index(request):
+    
+    data_dict = {}
+
+    list = Keyword.objects.all()
+    for ele in list:
+        if not ele.keyword in data_dict.keys():
+            data_dict[ele.keyword] = 0
+        else:
+            data_dict[ele.keyword] +=1
+    print data_dict
+
+    data = []
+    for ele in data_dict.keys():
+        new=[]
+        new.append(ele.encode("utf-8"))
+        new.append(data_dict[ele])
+        data.append(new)
+
+
+
     latest_article_list = Article.objects.order_by('date_added')
+    
 
     # output = ', '.join([p.title for p in latest_article_list])
     # return HttpResponse(output)
@@ -17,5 +41,5 @@ def index(request):
     # })
     # return HttpResponse(template.render(context))
 
-    context = {'latest_article_list': latest_article_list}
+    context = {'latest_article_list': latest_article_list,'data': data}
     return render(request, 'articles/index.html', context)
