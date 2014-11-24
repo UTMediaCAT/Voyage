6abd1ad1e99f46a7bf77af45ab36181cfed811c3
Jian Heng Pan <panjian2@iits-b473-20077.(none)>
2014-11-23 20:51:11 -0500

    update warc_generator
    has better warc api buildin in terminal

--- src/CreateWarc.sh
+++ src/CreateWarc.sh
@@ -1,12 +1,9 @@
 #!/bin/bash 
 url=$1
 BASEDIR=$(dirname $0) 
-cd crawler
-python crawler.py -u $1
-cd ..
 cd ..
 mkdir -p warc
 mkdir -p warc/$3
-mv ./src/crawler/out.warc.gz ./warc/$3
-cd ./warc/$3 
-mv out.warc.gz $2'.warc.gz'
+cd warc/$3
+wget $url --warc-file=$2
+rm index.html 2> /dev/null 
