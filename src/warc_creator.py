4c107d40273ee1caa4173eba4374c019266a362e
ryanbelt <ryanbelt1993129@Hotmail.com>
2014-11-23 21:41:13 -0500

    Update warc_creator.py
    now is avalable to crawl twitter and article

--- src/warc_creator.py
+++ src/warc_creator.py
@@ -1,15 +1,35 @@
 import os
 import subprocess
 
-def create_warc(url,folderName):
+ARTICLE_WARC_DIR = "warc/article"
+TWITTER_WARC_DIR = "warc/twitter"
+
+def create_article_warc(url):
     '''
-    (url,Folder_Name)-->None
-    giving url and type(article/twitter) it will export warc file
+    (url)-->None
+    giving url it will export warc file
     
-    create_warc("http://www.facebook.com","article")
+    create_warc("http://www.facebook.com")
     it should have a warc.gz file under 
-    ./warc/article/http:\\www.facebook.com.warc.gz
+    ARTICLE_WARC_DIR/http:\\www.facebook.com.warc.gz
     '''
     rename_url=url.replace("/","\\")
-    os.chmod('./CreateWarc.sh', 0700)
-    subprocess.call(['./CreateWarc.sh',url,rename_url,folderName])
+    os.chmod('./CreateArticleWarc.sh', 0700)
+    subprocess.call(['./CreateArticleWarc.sh',url,rename_url,ARTICLE_WARC_DIR])
+    
+def create_twitter_warc(url):
+    '''
+    (url)-->None
+    giving url it will export warc file
+    
+    create_warc("https://twitter.com/LeagueOfLegends")
+    it should have a HTML file under 
+    TWITTER_WARC_DIR/LeagueOfLegends.html
+    '''
+    rename_url=url.replace("/","\\")
+    url_split=url.split("/")
+    rename_html=url_split[len(url_split)-1]
+    print(rename_html)
+    os.chmod('./CreateTwitterWarc.sh', 0700)
+    subprocess.call(['./CreateTwitterWarc.sh',url,rename_url,rename_html,TWITTER_WARC_DIR])    
+
