a97f7184e856000b5043722f5638b922e7eba9cf
Jian Heng Pan <panjian2@iits-b473-20077.(none)>
2014-11-23 19:35:57 -0500

    edit warc sh and py
    now it can create folder

--- src/CreateWarc.sh
+++ src/CreateWarc.sh
@@ -6,6 +6,7 @@ python crawler.py -u $1
 cd ..
 cd ..
 mkdir -p warc
-mv ./src/crawler/out.warc.gz ./warc/
-cd ./warc 
+mkdir -p warc/$3
+mv ./src/crawler/out.warc.gz ./warc/$3
+cd ./warc/$3 
 mv out.warc.gz $2'.warc.gz'
--- src/warc_creator.py
+++ src/warc_creator.py
@@ -1,7 +1,15 @@
 import os
 import subprocess
 
-def create_warc(url):
+def create_warc(url,folderName):
+    '''
+    (url,Folder_Name)-->None
+    giving url and type(article/twitter) it will export warc file
+    
+    create_warc("http://www.facebook.com","article")
+    it should have a warc.gz file under 
+    ./warc/article/http:\\www.facebook.com.warc.gz
+    '''
     rename_url=url.replace("/","\\")
     os.chmod('./CreateWarc.sh', 0700)
-    subprocess.call(['./CreateWarc.sh',url,rename_url])     
+    subprocess.call(['./CreateWarc.sh',url,rename_url,folderName])
