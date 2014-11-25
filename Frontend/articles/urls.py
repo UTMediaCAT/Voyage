90f5044532afb747a96963014f50ca5c0f521d48
Yuya Iwabuchi <yuya.iwabuchi@gmail.com>
2014-11-25 01:30:13 -0500

    Removed index

--- Frontend/articles/urls.py
+++ Frontend/articles/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 from articles import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
     url(r'^getJson', views.getJson, name='getJson'),
     url(r'^warc/(?P<filename>.+)$', views.getWarc, name='getWarc'),
 )
