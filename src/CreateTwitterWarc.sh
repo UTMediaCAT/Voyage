7d2c67a1360dc15c1c214854f18afa57c19d62a9
Yuya Iwabuchi <yuya.iwabuchi@gmail.com>
2014-11-24 19:59:03 -0500

    Fixed warc duplicating bug and richer db

--- src/CreateTwitterWarc.sh
+++ src/CreateTwitterWarc.sh
@@ -4,6 +4,11 @@ BASEDIR=$(dirname $0)
 cd ..
 mkdir -p $4
 cd $4
-wget $url --warc-file=$2 2> /dev/null 
-rm $2".warc.gz" 2> /dev/null 
-mv $3 $2".html"
+wget $url --warc-file=$2 2> /dev/null
+rm $2'.warc.gz' 2> /dev/null
+if [ -f $2'.html' ]:
+then
+    cp -fp $3 $2'.html' 2> /dev/null
+else
+    mv $3 $2'.html' 2> /dev/null
+fi
