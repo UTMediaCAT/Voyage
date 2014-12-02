7d14b6d8fa5d055d69672e28e9de7b11c1d7b42d
Jian Heng Pan <panjian2@ic406-137.(none)>
2014-12-01 20:04:38 -0500

    fully update PEP8 for test_executer and test_warc_creator

--- src/unit_tests/test_warc_creator.py
+++ src/unit_tests/test_warc_creator.py
@@ -8,9 +8,11 @@ import unittest
 import time
 
 
-ARTICLE_HTML = "http://www.cbc.ca/news/world/nor-easter-storm-blasts-eastern-u-s-with-heavy-rain-and-snow-1.2851711"
+ARTICLE_HTML = "http://www.cbc.ca/news/world/nor-easter-storm-blasts-" + \
+                "eastern-u-s-with-heavy-rain-and-snow-1.2851711"
 TWITTER_HTML = "https://twitter.com/wesbos/status/519123918422958081"
-WRONG_ARTICLE_HTML = "http://www.cbc.ca/news/world/nor-easter-storm-blasts-eastern-u-s-with"
+WRONG_ARTICLE_HTML = "http://www.cbc.ca/news/world/nor-easter" + \
+                     "-storm-blasts-eastern-u-s-with"
 WRONG_TWITTER_HTML = "https://twitter.com/wesbos/statu"
 WARC_ARTICLE_DIRECTORY = "warc/article"
 WARC_TWITTER_DIRECTORY = "warc/twitter"
@@ -27,14 +29,18 @@ class TestWarcCreator(unittest.TestCase):
         """
         try:
             os.chdir("..")
-            os.remove(WARC_ARTICLE_DIRECTORY+"/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern-u-s-with-heavy-rain-and-snow-1.2851711.warc.gz")
+            os.remove(WARC_ARTICLE_DIRECTORY + \
+                "/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-" + \
+                "eastern-u-s-with-heavy-rain-and-snow-1.2851711.warc.gz")
         except OSError:
             pass      
         self.setUp()
         wc.create_article_warc(ARTICLE_HTML)
         os.chdir("..")
         time.sleep(1)
-        self.assertTrue(os.path.isfile(WARC_ARTICLE_DIRECTORY+"/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern-u-s-with-heavy-rain-and-snow-1.2851711.warc.gz") )
+        self.assertTrue(os.path.isfile(WARC_ARTICLE_DIRECTORY + \
+            "/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern" + \
+            "-u-s-with-heavy-rain-and-snow-1.2851711.warc.gz") )
         
     def test_create_twitter_warc(self):
         """
@@ -42,14 +48,16 @@ class TestWarcCreator(unittest.TestCase):
         """
         try:
             os.chdir("..")
-            os.remove(WARC_TWITTER_DIRECTORY+"/https:__twitter.com_wesbos_status_519123918422958081.warc.gz")
+            os.remove(WARC_TWITTER_DIRECTORY + \
+                "/https:__twitter.com_wesbos_status_519123918422958081.warc.gz")
         except OSError:
             pass   
         self.setUp()
         wc.create_twitter_warc(TWITTER_HTML)
         os.chdir("..")
         time.sleep(1)
-        self.assertTrue(os.path.isfile(WARC_TWITTER_DIRECTORY+"/https:__twitter.com_wesbos_status_519123918422958081.warc.gz") ) 
+        self.assertTrue(os.path.isfile(WARC_TWITTER_DIRECTORY + \
+            "/https:__twitter.com_wesbos_status_519123918422958081.warc.gz") ) 
         
         
     def test_create_wrong_url_article_warc(self):
@@ -59,30 +67,36 @@ class TestWarcCreator(unittest.TestCase):
         """        
         try:
             os.chdir("..")
-            os.remove(WARC_ARTICLE_DIRECTORY+"/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern-u-s-with.warc.gz")
+            os.remove(WARC_ARTICLE_DIRECTORY + \
+                "/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern" + \
+                "-u-s-with.warc.gz")
         except OSError:
             pass      
         self.setUp()
         wc.create_article_warc(WRONG_ARTICLE_HTML)
         os.chdir("..")
         time.sleep(1)
-        self.assertTrue(os.path.isfile(WARC_ARTICLE_DIRECTORY+"/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern-u-s-with.warc.gz") )  
+        self.assertTrue(os.path.isfile(WARC_ARTICLE_DIRECTORY + \
+            "/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern" + \
+            "-u-s-with.warc.gz") )  
     
     def test_create_wrong_url_twitter_warc(self):
         """
-        crawling a wrong twitter article should be going to success because there is 
-        404 page
+        crawling a wrong twitter article should be going to success because there 
+        is 404 page
         """
         try:
             os.chdir("..")
-            os.remove(WARC_TWITTER_DIRECTORY+"/https:__twitter.com_wesbos_statu.warc.gz")
+            os.remove(WARC_TWITTER_DIRECTORY + \
+                "/https:__twitter.com_wesbos_statu.warc.gz")
         except OSError:
             pass   
         self.setUp()
         wc.create_twitter_warc(WRONG_TWITTER_HTML)
         os.chdir("..")
         time.sleep(1)
-        self.assertTrue(os.path.isfile(WARC_TWITTER_DIRECTORY+"/https:__twitter.com_wesbos_statu.warc.gz") )    
+        self.assertTrue(os.path.isfile(WARC_TWITTER_DIRECTORY + \
+            "/https:__twitter.com_wesbos_statu.warc.gz") )    
         
     def test_create_exist_twitter_warc(self):
         """
@@ -90,7 +104,8 @@ class TestWarcCreator(unittest.TestCase):
         """
         wc.create_twitter_warc(TWITTER_HTML)
         os.chdir("..")
-        self.assertTrue(os.path.isfile(WARC_TWITTER_DIRECTORY+"/https:__twitter.com_wesbos_status_519123918422958081.warc.gz")) 
+        self.assertTrue(os.path.isfile(WARC_TWITTER_DIRECTORY + \
+            "/https:__twitter.com_wesbos_status_519123918422958081.warc.gz")) 
         
     def test_create_exist_article_warc(self):
         """
@@ -98,7 +113,9 @@ class TestWarcCreator(unittest.TestCase):
         """     
         wc.create_article_warc(ARTICLE_HTML)
         os.chdir("..")
-        self.assertTrue(os.path.isfile(WARC_ARTICLE_DIRECTORY+"/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-eastern-u-s-with-heavy-rain-and-snow-1.2851711.warc.gz") )
+        self.assertTrue(os.path.isfile(WARC_ARTICLE_DIRECTORY + \
+            "/http:__www.cbc.ca_news_world_nor-easter-storm-blasts-" + \
+            "eastern-u-s-with-heavy-rain-and-snow-1.2851711.warc.gz") )
         
 
 
