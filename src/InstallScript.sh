ef92e3c5dd7f24f75986152e290bb00da3d42fc5
wangx173 <rogerxiang.wang@gmail.com>
2015-05-28 15:34:29 -0400

    Update InstallScript.sh
    deleted pymongo install command

--- src/InstallScript.sh
+++ src/InstallScript.sh
@@ -7,7 +7,6 @@ python get-pip.py --user
 rm get-pip.py
 cd $prevdir
 
-pip install --user pymongo==2.7.2
 pip install --user Django==1.7.1
 pip install --user newspaper==0.0.8
 pip install --user tweepy==2.3.0
