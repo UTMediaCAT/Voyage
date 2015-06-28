4312e4689eb96e0d5b95de2e073ba75007cc066e
root <root@hacks.hacks.c8.internal.cloudapp.net>
2015-06-28 12:44:05 -0400

    use sudo to install

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-apt-get update
-apt-get install -y python-pip python-dev libxml2-dev libxslt1-dev zlib1g-dev wget
+sudo apt-get update
+sudo apt-get install -y python-pip python-dev libxml2-dev libxslt1-dev zlib1g-dev wget
 
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
