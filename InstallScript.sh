72faed10837e91e11b1ceeb5ab6c94eca943f51c
Yuya Iwabuchi <yuya.iwabuchi@gmail.com>
2015-07-13 13:12:54 -0400

    Installscript now stores project directory

--- InstallScript.sh
+++ InstallScript.sh
@@ -5,3 +5,7 @@ sudo apt-get update
 sudo apt-get install -y python-pip python-dev libxml2-dev libxslt1-dev zlib1g-dev libjpeg-dev
 
 sudo pip install -r requirements.txt
+
+dir=`pwd`
+sed 's#projectdir:.*$#projectdir: '"${dir}"'#' config.yaml > tmp.yaml
+mv tmp.yaml config.yaml
