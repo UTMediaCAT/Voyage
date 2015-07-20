43abb5a6ee9e90963a44dad8c770b2626d3006f7
wangx173 <wangx173@wifihost-102-56.wireless.utsc.utoronto.ca>
2015-07-20 18:34:36 -0400

    modified ServerBackup.sh

--- src/ServerBackup.sh
+++ src/ServerBackup.sh
@@ -10,13 +10,25 @@ cd ../Frontend
 case "${args[0]}" in 
 	"add")
 		DATE=`date +%Y-%m-%d:%H:%M:%S`
-		sqlite3 db.sqlite3 ".backup DatabaseBackup/bk_$DATE.sqlite3";;
+		sqlite3 db.sqlite3 ".backup DBbackup/db.sqlite3.$DATE";;
 
 	"remove")
 		if [ "${args[1]}" == "" ]; then
 			echo "Usage: ./ServerBackup.sh [remove] [days]    	Remove the backup file older than [days] days"
 		else 
-			find ./DatabaseBackup -mtime +"${args[1]}"  -type f -delete
+
+
+			for old in `ls ./DBbackup`
+			do
+				old_day=`echo $old  | cut -c 12-28`
+
+				if [`TZ=EST date -d "$old_day" +%Y-%m-%d:%H:%M:%S` -lt `TZ=EST date -d "${args[1]} days ago"  +%Y-%m-%d:%H:%M:%S` ]; then 
+					rm -r "./DBbackup/$old"
+				fi
+
+
+			done
+
 		fi ;;
 	*)
 		echo "Usage:"
