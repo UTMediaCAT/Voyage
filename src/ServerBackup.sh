d1c21d4cb1aa279891ec96f4b3aa7645dd9c39a8
wangx173 <wangx173@wifihost-102-56.wireless.utsc.utoronto.ca>
2015-07-22 17:35:24 -0400

    fixed ServerBackup.sh

--- src/ServerBackup.sh
+++ src/ServerBackup.sh
@@ -9,7 +9,7 @@ cd $BASEDIR
 cd ../Frontend
 case "${args[0]}" in 
 	"add")
-		DATE=`date +%Y-%m-%d:%H:%M:%S`
+		DATE=`date +'%Y-%m-%d %H:%M:%S'`
 		sqlite3 db.sqlite3 ".backup DBbackup/db.sqlite3.$DATE";;
 
 	"remove")
@@ -22,7 +22,7 @@ case "${args[0]}" in
 			do
 				old_day=`echo $old  | cut -c 12-28`
 
-				if [`TZ=EST date -d "$old_day" +%Y-%m-%d:%H:%M:%S` -lt `TZ=EST date -d "${args[1]} days ago"  +%Y-%m-%d:%H:%M:%S` ]; then 
+				if [date -d "$old_day" + %s -lt date -d "${args[1]} days ago"  + %s  ]; then 
 					rm -r "./DBbackup/$old"
 				fi
 
