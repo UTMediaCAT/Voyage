822874183c1587922581ec9b241f20fdf8b0ea23
wangx173 <wangx173@wifihost-102-56.wireless.utsc.utoronto.ca>
2015-07-22 17:52:36 -0400

    bug fixed in ServerBackup.sh

--- src/ServerBackup.sh
+++ src/ServerBackup.sh
@@ -9,7 +9,7 @@ cd $BASEDIR
 cd ../Frontend
 case "${args[0]}" in 
 	"add")
-		DATE=`date +'%Y-%m-%d %H:%M:%S'`
+		DATE=`date +'%Y-%m-%d_%H:%M:%S'`
 		sqlite3 db.sqlite3 ".backup DBbackup/db.sqlite3.$DATE";;
 
 	"remove")
@@ -20,9 +20,9 @@ case "${args[0]}" in
 
 			for old in `ls ./DBbackup`
 			do
-				old_day=`echo $old  | cut -c 12-28`
+				old_day=`echo $old  | cut -c 12-28 | tr _ " "`
 
-				if [`date -d "$old_day" +%s` -lt `date -d "${args[1]} days ago"  +%s`  ]; then 
+				if [`date -d "$old_day " +%s` -lt `date -d "${args[1]} days ago"  +%s`  ]; then 
 					rm -r "./DBbackup/$old"
 				fi
 
