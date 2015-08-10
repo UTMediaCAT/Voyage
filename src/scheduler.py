761ba0fa6fb2e05763d99557c6f3e6548cc6ff35
wangx173 <wangx173@wifihost-102-56.wireless.utsc.utoronto.ca>
2015-08-10 12:27:36 -0400

    scheduler added

--- src/scheduler.py
+++ src/scheduler.py
@@ -2,13 +2,21 @@ __author__ = 'wangx173'
 import schedule
 import time
 import Caching
+import subprocess
 
+
+
+def backup_db_add ():
+    subprocess.call(["./ServerBackup.sh","add"])
+
+def backup_db_remove ():
+    subprocess.call(["./ServerBackup.sh","remove","10"])
 
 def  setup():
-
     schedule.every(10).minutes.do(Caching.setArticleCachedData)
     schedule.every(10).minutes.do(Caching.setTweetCachedData)
-
+    schedule.every(1).days.do(backup_db_add)
+    schedule.every(1).days.do(backup_db_remove)
 
 
 setup()
