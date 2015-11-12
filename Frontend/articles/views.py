aa5fe2dd6ab5106ce587c8a933e8a5c7da69064c
wangx173 <wangx173@WangXiangs-MBP.lan>
2015-11-12 03:40:58 -0500

    download pdf

--- Frontend/articles/views.py
+++ Frontend/articles/views.py
@@ -44,8 +44,7 @@ def getWarc(request, filename):
 
     config = common.get_config()['warc']
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../', config['dir'] + "/" + config['article_subdir']))
-    filename_ext = path + "/" + filename + ".tar"
-    subprocess.call(["tar", "cvzf", filename + ".tar", filename +".png", filename + ".pdf", "--force-loca"], cwd=path + "/")
+    filename_ext = path + "/" + filename + ".pdf"
     warc = open(filename_ext, "rb")
     res = HttpResponse(warc, content_type="application/force-download")
     warc.close()
@@ -53,14 +52,14 @@ def getWarc(request, filename):
     # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
     if u'WebKit' in request.META['HTTP_USER_AGENT']:
         # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-        filename_header = 'filename=%s' % (filename + ".tar").encode('utf-8')
+        filename_header = 'filename=%s' % (filename + ".pdf").encode('utf-8')
     elif u'MSIE' in request.META['HTTP_USER_AGENT']:
         # IE does not support internationalized filename at all.
         # It can only recognize internationalized URL, so we do the trick via routing rules.
         filename_header = ''
     else:
         # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
-        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote((filename + ".tar").encode('utf-8'))
+        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote((filename + ".pdf").encode('utf-8'))
     res['Content-Disposition'] = 'attachment; ' + filename_header
     
     return res
