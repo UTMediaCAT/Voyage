23e4caacdda042940ee40f82e4716d46ce9de35b
zhouwein <william.zhou@mail.utoronto.ca>
2015-11-19 11:46:07 -0500

    return newspaper text if possible rather than readability

--- src/ExplorerArticle.py
+++ src/ExplorerArticle.py
@@ -115,15 +115,15 @@ class ExplorerArticle(object):#derive from object for getters/setters
     @property
     def text(self, strip_html=False):
         if(strip_html):
-            if(self._readability_text):
+            if(self._newspaper_text):
+                return self._newspaper_text
+            else:
                 try:
                     return lxml.html.fromstring(self._readability_text).text_content()
                 except lxml.etree.Error as e:
                     return ""
-            else:
-                return self._newspaper_text
         else:
-            return self._readability_text or self._newspaper_text
+            return self._newspaper_text or self._readability_text
 
 
     def get_links(self, article_text_links_only=False):
