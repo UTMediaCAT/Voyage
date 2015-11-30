1c9f5aecfcca041f9abe77b97956c42e2d225007
wangx173 <rogerxiang.wang@gmail.com>
2015-11-30 13:59:56 -0500

    added pdf in config file

--- Frontend/articles/views.py
+++ Frontend/articles/views.py
@@ -44,7 +44,7 @@ def getWarc(request, filename):
 
     config = common.get_config()['warc']
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../', config['dir'] + "/" + config['article_subdir']))
-    filename_ext = path + "/" + filename + ".pdf"
+    filename_ext = path + "/" + filename + ".warc"
     warc = open(filename_ext, "rb")
     res = HttpResponse(warc, content_type="application/force-download")
     warc.close()
@@ -52,14 +52,14 @@ def getWarc(request, filename):
     # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
     if u'WebKit' in request.META['HTTP_USER_AGENT']:
         # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-        filename_header = 'filename=%s' % (filename + ".pdf").encode('utf-8')
+        filename_header = 'filename=%s' % (filename + ".warc").encode('utf-8')
     elif u'MSIE' in request.META['HTTP_USER_AGENT']:
         # IE does not support internationalized filename at all.
         # It can only recognize internationalized URL, so we do the trick via routing rules.
         filename_header = ''
     else:
         # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
-        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote((filename + ".pdf").encode('utf-8'))
+        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote((filename + ".warc").encode('utf-8'))
     res['Content-Disposition'] = 'attachment; ' + filename_header
     
     return res
@@ -67,7 +67,7 @@ def getWarc(request, filename):
     
 def getPDF(request, filename):
 
-    config = common.get_config()['warc']
+    config = common.get_config()['pdf']
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../', config['dir'] + "/" + config['article_subdir']))
     filename_ext = path + "/" + filename + ".pdf"
     pdf = open(filename_ext, "rb")
