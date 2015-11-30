cca1370ba5543f06c29ef7156611108f52c829e5
wangx173 <rogerxiang.wang@gmail.com>
2015-11-30 12:14:03 -0500

    fixed bug that pdfs do not open with browser

--- Frontend/articles/views.py
+++ Frontend/articles/views.py
@@ -74,17 +74,4 @@ def getPDF(request, filename):
     res = HttpResponse(pdf, mimetype="application/pdf")
     pdf.close()
 
-    # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
-    if u'WebKit' in request.META['HTTP_USER_AGENT']:
-        # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-        filename_header = 'filename=%s' % (filename + ".pdf").encode('utf-8')
-    elif u'MSIE' in request.META['HTTP_USER_AGENT']:
-        # IE does not support internationalized filename at all.
-        # It can only recognize internationalized URL, so we do the trick via routing rules.
-        filename_header = ''
-    else:
-        # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
-        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote((filename + ".pdf").encode('utf-8'))
-    res['Content-Disposition'] = 'attachment; ' + filename_header
-    
     return res
