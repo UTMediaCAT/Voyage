00d27d5581afcef16e4b4dfc2bda8c0c53b558b9
wangx173 <rogerxiang.wang@gmail.com>
2015-12-01 17:57:51 -0500

    Changed wpull command

--- src/warc_creator.py
+++ src/warc_creator.py
@@ -19,7 +19,7 @@ def create_warc(url, dir):
     rename_url = url.replace("/", "_")
     logging.info("creating warc \"{0}\" in \"{1}\"".format(rename_url, dir))
     subprocess.call(["mkdir", "-p", dir], cwd="..", close_fds=True)
-    subprocess.Popen(["wpull", "--user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36", "--no-robots", "--no-check-certificate", "--no-cookies", "--timeout", "20", "--session-timeout", "20", "--phantomjs", "--phantomjs-max-time","150", "--warc-file",  rename_url,  url], cwd="../"+dir, close_fds=True, stderr=subprocess.PIPE)
+    subprocess.Popen(["wpull", "--user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36", "--no-robots", "--no-check-certificate", "--no-cookies", "--timeout", "20", "--session-timeout", "20", "--phantomjs", "--no-phantomjs-snapshot", "--phantomjs-max-time","150", "--warc-file",  rename_url,  url], cwd="../"+dir, close_fds=True, stderr=subprocess.PIPE)
 
 
 def create_pdf(url, dir):
