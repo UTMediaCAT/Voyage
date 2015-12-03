0063d53ea01ce5e9173df84e07e20745c9940faf
Jai Sughand <jaisughand@gmail.com>
2015-12-03 05:14:29 -0500

    changed wait to sleep instead in warc_queue

--- src/warc_queue.py
+++ src/warc_queue.py
@@ -10,12 +10,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Frontend.settings'
 django.setup()
 
+import time
 import commands
 import warc_creator
 
 
 if __name__ == "__main__":
-	max_phantoms = 8
+	max_phantoms = 4
 	wait_time = 2
 	article_file_name = "article_warc.stream"
 
@@ -23,9 +24,8 @@ if __name__ == "__main__":
 	while (True):
 		while (commands.getoutput('ps').count('phantomjs') >= max_phantoms):
 			print(commands.getoutput('ps').count('phantomjs'))
-			wait(wait_time)
+			time.sleep(wait_time)
 
-		print(article_queue)
 		article_file = open(article_file_name, "r+")
 		for url in article_file:
 			article_queue.append(url.strip())
@@ -33,6 +33,7 @@ if __name__ == "__main__":
 		article_file.truncate()
 		article_file.close()
 
+		print(article_queue[0])
 		url = article_queue.pop(0)
 		warc_creator.create_article_warc(url)
 		#warc_creator.create_article_pdf(url)
