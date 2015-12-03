42106633b75fbf2476548b1a6486cc4bb4ef95b9
zhouwein <william.zhou@mail.utoronto.ca>
2015-12-03 14:03:14 -0500

    command line test for testing css selectors

---
+++ src/TestSelector.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
+                                             'Frontend')))
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Frontend.settings'
+# For Models connecting with the Django Database
+from explorer.models import *
+from ExplorerArticle import ExplorerArticle
+
+if __name__ == "__main__":
+    id = input("site id: ")
+    django.setup()
+    site = ReferringSite.objects.get(pk=id)
+    url = input("url: ")
+    article = ExplorerArticle(url)
+    article.download()
+    article.preliminary_parse()
+    article.newspaper_parse()
+
+    fields = {}
+    for css in site.referringsitecssselector_set.all():
+        if not (css.field_choice in fields.keys()):
+            fields[css.field_choice] = []
+
+        fields[css.field_choice].append({'pattern': css.pattern, 'regex': css.regex})
+    if(not len(fields.keys())):
+        print "no fields"
+
+    for key, value in fields.iteritems():
+        print "field \"{0}\"".format(key)
+        print article.evaluate_css_selectors(value)
+        print
