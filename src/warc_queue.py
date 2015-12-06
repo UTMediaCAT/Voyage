7f1738274330b9604c079de3166f27800ccf9c2f
wangx173 <rogerxiang.wang@gmail.com>
2015-12-05 20:14:21 -0500

    added function to retry all failed tasks when the task_queue is empty

--- src/warc_queue.py
+++ src/warc_queue.py
@@ -17,9 +17,8 @@ import warc_creator
 
 
 if __name__ == "__main__":
-	max_phantoms = 5
+	max_phantoms = 2
 	wait_time = 5
-	article_file_name = "article_warc.stream"
 
 	article_queue = []
 	article_processes = []
@@ -27,6 +26,11 @@ if __name__ == "__main__":
 		while (len(article_processes) >= max_phantoms):
 			article_processes[:] = [p for p in article_processes if p.poll() is None]
 			time.sleep(wait_time)
+
+		article_file_name = "article_warc.stream"
+		#if not tasks remaining, retry thoes url that fialed before
+		if os.stat(article_file_name).st_size == 0:
+			article_file_name = "article_warc.stream.failure"
 
 		article_file = open(article_file_name, "r+")
 		for url in article_file:
@@ -51,13 +55,15 @@ if __name__ == "__main__":
 				# Waiting for the process to finish.
 				time.sleep(0.1)  # Avoid being a CPU busy loop.
 				num_polls += 1
-				if num_polls > 400:
+				if num_polls > 600:
 					p.terminate()
-					article_queue.append(url)
+					fail_name = "article_warc.stream.failure"
+					fail = open(fail_name, "a")
+					fail.write(url + "\n")
+					fail.close()
+					break
 
 			article_processes.append(p)
-
-
 
 		article_processes[:] = [p for p in article_processes if p.poll() is None]
 
