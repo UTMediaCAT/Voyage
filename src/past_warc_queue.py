fcf3a5e1560bf697a8d9eee63306d51d26ecc1fd
wangx173 <rogerxiang.wang@gmail.com>
2016-08-04 02:16:20 -0400

    fixed past_warc_queue

--- src/past_warc_queue.py
+++ src/past_warc_queue.py
@@ -6,14 +6,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
                                              'Frontend')))
 
-import commands
-import django
+import warc_creator
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Frontend.settings'
 from articles.models import*
 
 article_file_name = "article_warc.stream"
 
 article_file = open(article_file_name, "a")
-for article in Version.objects.all():
-	article_file.write(article.url.encode("utf-8") + "\n")
+for version in Version.objects.all():
+     warc_creator.enqueue_article(Article.objects.get(id = version.article_id), version.text_hash)
 article_file.close()
