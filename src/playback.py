f6bbe598b173fe00ad94e0caaed64f95cc538226
zhouwein <william.zhou@mail.utoronto.ca>
2016-09-21 10:26:32 -0400

    .

--- src/playback.py
+++ src/playback.py
@@ -3,10 +3,10 @@ import time
 import sys
 import collections
 
-def assertEqualStr(a,b):
+def assertEqual(a,b):
     if(a != b):
-        print("expected: " + a)
-        print("got: " + b)
+        print("expected: " + str(a))
+        print("got: " + str(b))
         assert False
 
 def replay_memory(stream):
@@ -15,10 +15,10 @@ def replay_memory(stream):
     visited.add("dummy")
     for line in stream:
         if(line[0] == '1'):#pop from tovisit queue
-            assert tovisit.pop() == line[1:]
+            assertEqual(tovisit.pop(), line[1:])
         elif(line[0] == '2'):#check if url exists
             expected_value = line[1] == 'y'
-            assert (line[2:] in visited) == expected_value
+            assertEqual((line[2:] in visited), expected_value)
         elif(line[0] == '3'):#insert into visited and tovisit
             tovisit.appendleft(line[1:])
             visited.add(line[1:])
@@ -46,13 +46,13 @@ def replay_postgres(stream):
             current_url = row[1]
             cursor.execute("DELETE FROM " + tovisit_table + " WHERE id=%s", (row_id,))
 
-            assert current_url == line[1:]
+            assertEqual(current_url, line[1:])
         elif(line[0] == '2'):#check if url exists
             expected_value = line[1] == 'y'
 
             cursor.execute(u"SELECT EXISTS(SELECT * FROM " + visited_table + " WHERE url=%s)",(line[2:],))
             exists = bool(cursor.fetchone()[0])
-            assert exists == expected_value
+            assertEqual(exists, expected_value)
         elif(line[0] == '3'):#insert into visited and tovisit
             cursor.execute(u"INSERT INTO " + tovisit_table + u" VALUES (DEFAULT , %s)", (line[1:],))
             cursor.execute(u"INSERT INTO " + visited_table + u" VALUES (%s)", (line[1:],))
