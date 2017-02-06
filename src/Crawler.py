e0181192e20536d486624e838fc07183b088e39b
Yuya <yuya.iwabuchi@gmail.com>
2017-02-06 01:15:43 -0500

    fixing Crawler logic

--- src/Crawler.py
+++ src/Crawler.py
@@ -136,10 +136,10 @@ class Crawler(object):
                     logging.info(u"added {0} to the to_visit".format(url))
 
                     # Append the url to visited to remove duplicates
-                    self.visited.add(current_url)
+                    self.visited.add(url)
 
-                    # Update the Queue
-                    self.to_visit.task_done()
+                # Update the Queue
+                self.to_visit.task_done()
 
                 return article
         except StopIteration as e:
