5d793e59596e1b22a7d5af19c3b81414057a86c1
Jai Sughand <jaisughand@gmail.com>
2017-06-26 16:28:01 -0400

    Fixed and improved InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+# Install apt dependencies
+apt-get update && apt-get install -y python-dev python-pip python3-pip python-numpy zlib1g-dev libxml2-dev libxslt-dev libjpeg-dev libpq-dev libfontconfig postgresql postgresql-contrib language-pack-en htop lsof
+apt-get autoremove -y python-setuptools
+
+# Install python dependencies:
+pip install -U pip setuptools
+pip install -r requirements.txt
+pip3 install wpull
+pip3 install html5lib==0.9999999
+pip3 install psutil
+
+# Install phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv -f phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+rm -rf phantomjs-2.1.1-linux-x86_64
+rm phantomjs-2.1.1-linux-x86_64.tar.bz2
+
+# Set up
 set -e
 
 dir=`pwd`
