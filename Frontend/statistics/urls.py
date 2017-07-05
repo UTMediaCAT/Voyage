25e091ca15dee3e408b60299bbbd01d4e9be8552
LukeBi <luke.bichangyu@gmail.com>
2017-07-05 12:01:21 -0400

    Removed stats and visualizations files

--- Frontend/statistics/urls.py
+++ Frontend/statistics/urls.py
@@ -3,9 +3,5 @@ from django.conf.urls import patterns, url
 from statistics import views
 
 urlpatterns = patterns('',
-	# url(r'^articles_js', views.articles_js, name='articles_js'),
- #    url(r'^articles', views.articles, name='articles'),
- #    url(r'^tweets_js', views.tweets_js, name='tweets_js'),
- #    url(r'^tweets', views.tweets, name='tweets'),
  	url(r'^$', views.notAvailable, name='not_available'),
 )
--- Frontend/statistics/views.py
+++ Frontend/statistics/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render, redirect
 from django.template import RequestContext, loader
-from subprocess import Popen
-import sys, os, datetime, time, re
 
-path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../', 'src'))
-sys.path.append(path)
-import analyzer
-import Caching
 
 def notAvailable(request):
     if not request.user.is_authenticated():
