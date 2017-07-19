706bc9d9a0d13ff3c0670fb720ff88f48621ea19
root <luke.bichangyu@gmail.com>
2017-07-19 17:22:19 +0000

    Removed executer, and code using it in view.py

--- Frontend/explorer/views.py
+++ Frontend/explorer/views.py
@@ -31,38 +31,6 @@ def validate_site(site):
 
 def command(request):
     if request.method == 'POST':
-        if request.POST.get('acommand') == 'Run':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "article", "run"], cwd=path)
-
-        if request.POST.get('acommand') == 'Pause':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "article", "pause"], cwd=path)
-
-        if request.POST.get('acommand') == 'Stop':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "article", "stop"], cwd=path)
-
-        if request.POST.get('acommand') == '[F]Stop':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "article", "fstop"], cwd=path)
-
-        if request.POST.get('tcommand') == 'Run':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "twitter", "run"], cwd=path)
-
-        if request.POST.get('tcommand') == 'Pause':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "twitter", "pause"], cwd=path)
-
-        if request.POST.get('tcommand') == 'Stop':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "twitter", "stop"], cwd=path)
-
-        if request.POST.get('tcommand') == '[F]Stop':
-            path = os.path.dirname(os.path.realpath(__file__))
-            Popen(["python", path + "/../../src/executer.py", "twitter", "fstop"], cwd=path)
-
         if request.POST.get('url'):
         	return validate_site(request.POST.get('url'))
 
