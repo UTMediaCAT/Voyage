8ba353561ff6bbe104c2cccb89d01098f2b03f12
root <luke.bichangyu@gmail.com>
2017-09-10 18:11:55 +0000

    ON/OFF indicator using grep

--- Frontend/explorer/templatetags/explorer_tags.py
+++ Frontend/explorer/templatetags/explorer_tags.py
@@ -1,5 +1,16 @@
 from django import template
+from subprocess import Popen, PIPE
 
 register = template.Library()
+
+@register.simple_tag
+def get_article_run_status():
+    ps_process = Popen(["ps", "aux"], stdout=PIPE)
+    ps_output = ps_process.communicate()[0]
+
+    status = "OFF"
+    if "python article_explorer.py" in ps_output:
+	status = "ON"
+    return status
 
 
