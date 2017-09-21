b92493c5a943ebe2cc30dd2bd6f3ca0795eb0f9c
root <luke.bichangyu@gmail.com>
2017-09-21 20:22:39 +0000

    Added indicator for twitter_explorer.py

--- Frontend/explorer/templatetags/explorer_tags.py
+++ Frontend/explorer/templatetags/explorer_tags.py
@@ -5,12 +5,31 @@ register = template.Library()
 
 @register.simple_tag
 def get_article_run_status():
+    """
     ps_process = Popen(["ps", "aux"], stdout=PIPE)
     ps_output = ps_process.communicate()[0]
 
     status = "OFF"
     if "python article_explorer.py" in ps_output:
 	status = "ON"
+    return status
+    """
+    return is_process_running("article_explorer.py")
+
+
+@register.simple_tag
+def get_twitter_run_status():
+    return is_process_running("twitter_explorer.py")
+
+
+def is_process_running(program_name):
+    # get all running process by 'ps aux'
+    ps_process = Popen(["ps", "aux"], stdout=PIPE)
+    ps_output = ps_process.communicate()[0]
+
+    status = "OFF"
+    if ("python " + program_name) in ps_output:
+        status = "ON"
     return status
 
 
