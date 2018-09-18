7d16ac4374f276c26042d62db6c8e71abed80b10
Yasir Haque <yasir.haque@outlook.com>
2018-09-18 00:34:31 +0000

    fixed upload

--- Frontend/options/views.py
+++ Frontend/options/views.py
@@ -29,37 +29,53 @@ def downloads(request):
 
     deleted = False
     context = {}
+    result = "a"
     if request.method == 'POST':
         try:
+            result += "b"
             scopefile = request.FILES['scopefile']
             try:
+                result += "c"
                 version = scopefile.readline().strip()
                 db_version = common.get_config()['database']['version']
-                if (version.count(".") == 2 and version != db_version):
+                result += "d"
+                result += "DD"
+                result += str(version.count(bytes('.'.encode('utf-8'))))
+                result += "qqqqqq "
+                if (str(version.count(bytes('.'.encode('utf-8')))) == 2 and version != db_version):
+                    result += "d1"
                     result = format("Database schema version mismatch (Need: %s, Given: %s)" %
                                     (db_version, version))
+                    result += "d2"
                 else:
+                    result += "e"
                     # Backup Current Scope in a variable
                     out = StringIO()
+                    result += "f"
                     management.call_command('dumpdata', 'explorer', 'taggit', stdout=out)
+                    result += "g"
                     currentScope = out.getvalue()
                     out.close()
-                    
+                    result += '1'
                     # Delete Current Scope
                     deleteScope()
                     deleted = True
-
+                    result += ' 2'
                     # Replace Scope
                     tf = tempfile.NamedTemporaryFile(suffix='.json')
-                    tf.write(scopefile.read())
+                    tf.write(bytes(scopefile.read()))
                     tf.seek(0)
                     out = StringIO()
+                    result += ' 3'
                     management.call_command('loaddata', tf.name, stdout=out)
                     out.close()
                     tf.close()
                     result = "Success"
-            except:
-                result = "Failed"
+            except Exception as e:
+                #result = "Faiadsasdasdad"
+                result += repr(e)
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                result += str(exc_tb.tb_lineno)
                 if (deleted):
                     # Put the Current Scope back into db
                     tf = tempfile.NamedTemporaryFile(suffix='.json')
