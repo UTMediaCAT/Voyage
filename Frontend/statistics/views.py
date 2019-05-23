c120148ffced41af78e94eb8143cfa334ee40dfd
root <ray.rahmn@hotmail.com>
2019-05-23 06:29:16 +0000

    Updated the statistics views and urls.

--- Frontend/statistics/views.py
+++ Frontend/statistics/views.py
@@ -8,28 +8,6 @@ sys.path.append(path)
 import analyzer
 import Caching
 
-def articles(request):
-    if not request.user.is_authenticated():
-        return redirect('/admin/login/?next=%s' % request.path)
-
-    data = []
-    context = {'data': data}
-    return render(request, 'statistics/articles.html', context)
-
-def articles_js(request):
-    data = Caching.getCacheData("Article_Statistics.Json")
+def coming_soon_statistics(request):
     #print data
-    return render(request, 'statistics/articles_js.html', data)
-
-def tweets(request):
-    if not request.user.is_authenticated():
-        return redirect('/admin/login/?next=%s' % request.path)
-
-    data = []
-    context = {'data': data}
-    return render(request, 'statistics/tweets.html', context)
-
-def tweets_js(request):
-    data = Caching.getCacheData("Tweet_Statistics.Json")
-    #print data
-    return render(request, 'statistics/tweets_js.html', data)
+    return render(request, 'statistics/coming_soon.html')
