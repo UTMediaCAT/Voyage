9f81ad7007cc3b3368645380e14405e0d36353af
AlAndr04 <aandros04@gmail.com>
2020-08-31 17:55:48 -0400

    Removed the need of chromedriver.exe path

--- src/headless_selenium.py
+++ src/headless_selenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(executable_path=r'C:\Users\Alex\source\repos\SeleniumTest\packages\Selenium.Chrome.WebDriver.2.45\driver\chromedriver.exe', chrome_options=options)
+driver = webdriver.Chrome(chrome_options=options)
 web_name = 'https://www.dpa-international.com/'
 driver.get(web_name)
 """try:
