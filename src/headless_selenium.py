b838a5a85b11c296d2f2a837d4e84130c70a84f2
AlAndr04 <aandros04@gmail.com>
2020-09-03 19:51:22 -0400

    Added more websites and fixed more bugs

--- src/headless_selenium.py
+++ src/headless_selenium.py
@@ -5,15 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
 import time
-def get_articles_from_site(driver, web_name):
-    site_regex_secure = r'https://www.(.*)'
-    site_regex_insecure = r'http://www.(.*)'
-    page_name = re.search(site_regex_secure, web_name).group(1)
+def get_articles_from_site(driver, web_name, tag_name = "a", attribute = "href"):
+    #Regex to separate the website name and its domain
+    general_regex = r'://(www\.)?(.*)(\.[a-zA-Z]+)'
+    if "https" == web_name[0:5]:
+        site_regex = r'https'+general_regex
+    else:
+        site_regex = r'http'+general_regex
+    path_regex = "^/?[^/]+/[^/]+$"
+    groups = re.search(site_regex, web_name) 
+    page_name = groups.group(2)
+    domain = groups.group(3)
     if 'www.' in web_name:
         if 'https' in web_name:
-            shortened_web_name = 'https://'+page_name
+            shortened_web_name = 'https://'+page_name+domain
         elif 'http' in web_name:
-            shortened_web_name = 'http://'+page_name
+            shortened_web_name = 'http://'+page_name+domain
     else:
         shortened_web_name = web_name
     print(page_name, web_name)
@@ -23,43 +30,32 @@ def get_articles_from_site(driver, web_name):
         driver.find_element_by_css_selector(".mfp-content .popup .link-close-popup > *").click()
         wait = WebDriverWait(driver, 10)
         wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, ".mfp-content .popup")))
-
-    time.sleep(10)
-    cells = driver.find_elements_by_tag_name('a')
-    article_links = []
-    weed_out = []
+    cells = driver.find_elements_by_css_selector(tag_name)
+    article_links = set()
     print(len(cells))
     for cell in cells:
-        if cell.get_attribute("href") != None:
-            link = cell.get_attribute("href")
-        else:
-            link = cell.get_attribute("src")
-        if link and link != '' and page_name in link and link != shortened_web_name and link != web_name+"#":
-            article_links.append(link)
-        else:
-            weed_out.append(link)
-    print(len(article_links), article_links)
-    print(weed_out)
-    
+        link = cell.get_attribute(attribute)
+        if link:
+            if re.match(path_regex, link):
+                article_links.add(web_name+link)
+            elif link != '' and page_name in link and link != shortened_web_name and link != web_name+"#":
+                article_links.add(link)
+    print(len(article_links))
+    return article_links
 options = Options()
 # Comment this out if you want the headless mode to be disabled
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
 #####################################
 driver = webdriver.Chrome(chrome_options=options)
 web_name = 'https://www.dpa-international.com/'
-driver.get(web_name)
-cells = driver.find_elements_by_css_selector('.cell')
-article_links = []
-print(len(cells))
-for cell in cells:
-    article_links.append(web_name+cell.get_attribute("data-link"))
-print(article_links)
-web_name = 'https://www.indianexpress.com/'
-get_articles_from_site(driver, web_name)
-web_name = 'https://www.jadaliyya.com/'
-get_articles_from_site(driver, web_name)
-web_name = 'https://www.bbc.com/'
-get_articles_from_site(driver, web_name)
+dpa_set = get_articles_from_site(driver, web_name, ".cell", "data-link")
+dpa_set = dpa_set.union(get_articles_from_site(driver, web_name))
+print(dpa_set)
+website_list = ['https://www.cbc.ca','https://www.indianexpress.com/','https://www.jadaliyya.com/',
+                'https://www.bbc.com/','https://www.alarabiya.net']
+for web_name in website_list:
+    article_set = get_articles_from_site(driver, web_name)
+    print(article_set)
 driver.close()
 driver.quit()
