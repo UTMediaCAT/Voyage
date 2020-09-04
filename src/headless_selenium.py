7a3791fb1cddfe0f7630048fb05a00712f4df365
AlAndr04 <aandros04@gmail.com>
2020-09-03 20:25:09 -0400

    Fixed permission denied issue for cbc and added comments

--- src/headless_selenium.py
+++ src/headless_selenium.py
@@ -25,6 +25,9 @@ def get_articles_from_site(driver, web_name, tag_name = "a", attribute = "href")
         shortened_web_name = web_name
     print(page_name, web_name)
     driver.get(web_name)
+    # Uncomment this for debugging, this saves a screenshot of the current state
+    # of the browser
+    #driver.save_screenshot('./'+page_name+'.png')
     time.sleep(20)
     if driver.find_elements_by_css_selector(".mfp-content .popup"):
         driver.find_element_by_css_selector(".mfp-content .popup .link-close-popup > *").click()
@@ -36,16 +39,23 @@ def get_articles_from_site(driver, web_name, tag_name = "a", attribute = "href")
     for cell in cells:
         link = cell.get_attribute(attribute)
         if link:
+            # If the string is a path and not a full link, concatenate with the the website string.
             if re.match(path_regex, link):
                 article_links.add(web_name+link)
-            elif link != '' and page_name in link and link != shortened_web_name and link != web_name+"#":
+            # Checks for:
+            # 1. String not being empty
+            # 2. String containing the name of the website
+            # 3. String not being just the website or any of the shortened or lengthened versions
+            elif link != '' and page_name in link and link != shortened_web_name and link != web_name and link != web_name+"#":
                 article_links.add(link)
     print(len(article_links))
     return article_links
-options = Options()
+options = webdriver.ChromeOptions()
 # Comment this out if you want the headless mode to be disabled
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
+options.add_argument('user-agent={0}'.format(user_agent))
 #####################################
 driver = webdriver.Chrome(chrome_options=options)
 web_name = 'https://www.dpa-international.com/'
